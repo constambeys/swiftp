a6812cf9143e51f5267db5cc3b639e7c2c87807a
constambeys <timo0190@gmail.com>
2025-02-04 21:57:40 +0200

    fix

--- app/src/main/java/be/ppareit/swiftp/locale/EditActivity.java
+++ app/src/main/java/be/ppareit/swiftp/locale/EditActivity.java
@@ -23,25 +23,24 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.MenuItem;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import com.twofortyfouram.locale.sdk.client.ui.activity.AbstractAppCompatPluginActivity;
 
 import net.vrallev.android.cat.Cat;
 
 import be.ppareit.swiftp.FsSettings;
 import be.ppareit.swiftp.R;
 
-import static be.ppareit.swiftp.locale.SettingsBundleHelper.BUNDLE_BOOLEAN_RUNNING;
-import static be.ppareit.swiftp.locale.SettingsBundleHelper.generateBundle;
-import static be.ppareit.swiftp.locale.SettingsBundleHelper.getBundleRunningState;
+
 
 /**
  * Created by ppareit on 29/04/16.
  */
-public class EditActivity extends AbstractAppCompatPluginActivity {
+public class EditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,65 +49,8 @@ public class EditActivity extends AbstractAppCompatPluginActivity {
 
         setContentView(R.layout.locale_edit_layout);
 
-        CharSequence callingApplicationLabel = null;
-        try {
-            PackageManager pm = getPackageManager();
-            ApplicationInfo ai = pm.getApplicationInfo(getCallingPackage(), 0);
-            callingApplicationLabel = pm.getApplicationLabel(ai);
-        } catch (final PackageManager.NameNotFoundException e) {
-            Cat.e("Calling package couldn't be found%s", e); //$NON-NLS-1$
-        }
-        if (null != callingApplicationLabel) {
-            setTitle(callingApplicationLabel);
-        }
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setSubtitle(R.string.swiftp_name);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
-    @Override
-    public boolean isBundleValid(@NonNull Bundle bundle) {
-        return SettingsBundleHelper.isBundleValid(bundle);
-    }
-
-    @Override
-    public void onPostCreateWithPreviousResult(@NonNull Bundle previousBundle,
-                                               @NonNull String previousBlurb) {
-        if (!isBundleValid(previousBundle)) {
-            Cat.e("Invalid bundle received, repairing to default");
-            previousBundle = generateBundle(this, false);
-        }
-        boolean running = previousBundle.getBoolean(BUNDLE_BOOLEAN_RUNNING);
-        RadioButton radioButton =
-                (RadioButton) findViewById(running ? R.id.radio_server_running :
-                                R.id.radio_server_stopped);
-        radioButton.setChecked(true);
-    }
-
-    @Nullable
-    @Override
-    public Bundle getResultBundle() {
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_server_state_group);
-        int checkedId = radioGroup.getCheckedRadioButtonId();
-        boolean running = (checkedId == R.id.radio_server_running);
-
-        return generateBundle(this, running);
-    }
-
-    @NonNull
-    @Override
-    public String getResultBlurb(@NonNull Bundle bundle) {
-        boolean running = getBundleRunningState(bundle);
-        return running ? "Running" : "Stopped";
-    }
 }
